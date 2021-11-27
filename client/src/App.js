3a939c86221fec9a531e8aed0f4d379de4c9467b
AndrzejL98 <andrzejlegowik1@gmail.com>
2021-11-27 14:53:11 +0100

    routing update
    adding aboutPage,ratingPage,welcomePage,galleryPage

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,10 @@ import LoginScreen from "./components/screens/LoginScreen";
 import RegisterScreen from "./components/screens/RegisterScreen";
 import ForgotPasswordScreen from "./components/screens/ForgotPasswordScreen";
 import ResetPasswordScreen from "./components/screens/ResetPasswordScreen";
+import WelcomePage from "./components/screens/WelcomePage";
+import AboutPage from "./components/screens/AboutPage";
+import GalleryPage from "./components/screens/GalleryPage";
+import RatingPage from "./components/screens/RatingPage";
 
 const App = () => {
   return (
@@ -18,6 +22,11 @@ const App = () => {
           <PrivateRoute exact path="/" component={PrivateScreen} />
           <Route exact path="/login" component={LoginScreen} />
           <Route exact path="/register" component={RegisterScreen} />
+          <Route exact path="/welcome" component={WelcomePage} />
+          <Route exact path="/about" component={AboutPage} />
+          <Route exact path="/gallery" component={GalleryPage} />
+          <Route exact path="/rating" component={RatingPage} />
+
           <Route
             exact
             path="/forgotpassword"
---
+++ client/src/components/screens/GalleryPage.js
@@ -0,0 +1,9 @@
+const GalleryPage = () => {
+  return (
+    <div>
+      <h1>GalleryPage</h1>
+    </div>
+  );
+};
+
+export default GalleryPage;
