75ac9ebf95ddb0918e6e7f7139b76cb3f42a8b81
AndrzejL98 <andrzejlegowik1@gmail.com>
2022-01-15 20:07:31 +0100

    Version 1.0.0

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 const connenctDB = require("./database/db");
 const authRoutes = require("./routes/auth");
+const path = require("path");
 
 //middleware
 app.use(express.static("public"));
@@ -18,6 +19,9 @@ const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 app.listen(port, () => {
